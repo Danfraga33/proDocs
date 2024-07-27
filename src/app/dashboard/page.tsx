911e73bd212696d27b512c3a19f4f66d3a90eb5a
Daniel Fraga <dan.fraga33@gmail.com>
2024-07-27 22:07:07 +0100

    style: add button design for newly created projects

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,16 +16,21 @@ const Dashboard = async () => {
   const projects: Projects = await getAllProjects();
 
   return (
-    <div className="flex-center">
+    <div className="flex-center ">
       <Button asChild>
         <Link href="/dashboard/create/project">Create New Project</Link>
       </Button>
       {projects.map((project) => (
         <div key={project.id}>
-          <Link href={`/dashboard/projectPresentation/${project.id}`}>
-            {project.name}
-          </Link>
-          ;
+          <ul className="flex mt-2 justify-start">
+            <li>
+              <Button asChild variant="outline">
+                <Link href={`/dashboard/projectPresentation/${project.id}`}>
+                  {project.name}
+                </Link>
+              </Button>
+            </li>
+          </ul>
         </div>
       ))}
     </div>
