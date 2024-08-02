5dbb3134708bb26e0e21f09eb9cfc8d319d44e34
Daniel Fraga <dan.fraga33@gmail.com>
2024-08-02 21:32:10 +0100

    feat: add supabase auth  (Milestone)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import Image from 'next/image';
-import SignUpForm from '../lib/components/SignUpForm';
+import Header from "@/lib/components/Header";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
-	return (
-		<div>
-			<SignUpForm />
-		</div>
-	);
+export default async function Home() {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.getUser();
+  console.log(data.user);
+  return (
+    <div className="h-screen">
+      <Header />
+      <div className="flex-center h-full">
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
