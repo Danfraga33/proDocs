b0206c838f89ce07f6d6ec95cb4b21700406d992
Daniel Fraga <dan.fraga33@gmail.com>
2024-08-07 15:37:48 +0100

    fix: fix styling problem with header not staying at top

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,21 +1,32 @@
+"use client";
+import "../../../styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+
 import { ReactNode } from "react";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/layouts/Header";
 import { createClient } from "@/utils/supabase/client";
-
-export default async function DashboardLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  const supabase = createClient();
-  const { data, error } = await supabase.auth.getSession();
+import DashboardHeader from "@/components/DashboardHeader";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const path = usePathname();
 
   return (
-    <section className="flex flex-col h-screen">
-      <Header />
-      {data.session && <Sidebar />}
-      {children}
-    </section>
+    <div>
+      <DashboardHeader />
+      <div
+        className={cn(
+          "flex-center antialised grainy min-h-screen bg-gray-50 font-sans dark:bg-stone-800",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </div>
+    </div>
   );
 }
